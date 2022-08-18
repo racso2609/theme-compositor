32367529877031c94c6afbe021bb8136b23b22e8
racso2609 <oscaremiliolugo@gmail.com>
2022-08-18 00:11:56 -0400

    fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const { actionsMethods } = require("./utils/commandActions");
 const { configFile } = require("./utils/configFiles");
 const { actions } = require("./utils/flags");
-if (!configFile.activeTemplate) throw new Error("No template set");
+if (!confactionsigFile.activeTemplate) throw new Error("No template set");
 
 actions.forEach((action) => {
   const result = actionsMethods[action.type](action.body);
