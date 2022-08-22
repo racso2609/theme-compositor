8c3d77cf762647d7d290e4de3cd89bdf2bf2fe8e
racso2609 <oscaremiliolugo@gmail.com>
2022-08-22 09:49:11 -0400

    add setup script

---
+++ scripts/setup.sh
@@ -0,0 +1,20 @@
+INITIAL_PATH="$HOME"
+INSTALL_PATH=(".local" 'share' 'theme-compositor')
+
+for i in "${INSTALL_PATH[@]}"
+do
+  echo $i
+  INITIAL_PATH="$INITIAL_PATH/$i"
+  if [[ -f $INITIAL_PATH ]]
+  then
+    echo "create $i"
+    mkdir $INITIAL_PATH
+  fi
+done
+
+echo $INITIAL_PATH
+sudo cp -r ./* $INITIAL_PATH
+cd $INITIAL_PATH
+
+npm i 
+sudo npm link
