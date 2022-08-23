1a8945725b9bf94fa4f962abc1b121e2bb0b660a
racso2609 <oscaremiliolugo@gmail.com>
2022-08-22 22:01:21 -0400

    fix setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,7 +5,7 @@ for i in "${INSTALL_PATH[@]}"
 do
   echo $i
   INITIAL_PATH="$INITIAL_PATH/$i"
-  if [[ -f $INITIAL_PATH ]]
+  if [[ ! -f $INITIAL_PATH ]]
   then
     echo "create $i"
     mkdir $INITIAL_PATH
@@ -13,8 +13,9 @@ do
 done
 
 echo $INITIAL_PATH
+npm i 
+
 sudo cp -r ./* $INITIAL_PATH
 cd $INITIAL_PATH
 
-npm i 
 sudo npm link
